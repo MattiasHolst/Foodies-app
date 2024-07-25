2cdc69942c9804a510c2f7121afd15e336459ed2
Mattias <mattias@compare-it.se>
2024-07-25 14:21:39 +0200

    Init foodie app and added empty routes for now

---
+++ app/meals/page.tsx
@@ -0,0 +1,10 @@
+export default function MealsPage() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          Welcome to the Meals Page!
+        </h1>
+      </main>
+    );
+  }
+  
---
+++ app/meals/share/page.tsx
@@ -0,0 +1,10 @@
+export default function SharePage() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          Share your food!
+        </h1>
+      </main>
+    );
+  }
+  
