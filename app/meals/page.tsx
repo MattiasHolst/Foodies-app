8b9819bf962e09e1c3bb3d6ab0a832b6e8a95a1e
Mattias <mattias@compare-it.se>
2024-07-25 14:33:19 +0200

    Added links to all pages

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function MealsPage() {
     return (
       <main>
         <h1 style={{ color: 'white', textAlign: 'center' }}>
           Welcome to the Meals Page!
+          <p><Link href="/meals/share">Share</Link></p>
         </h1>
       </main>
     );
