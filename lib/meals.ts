42db2bccebb75e1fa7c05f9e7f9718586c99d9b3
Mattias <mattias@compare-it.se>
2024-07-28 14:59:46 +0200

    Added a lib folder to handle DB calls, able to fetch all meals from DB and showing a suspense fallback while fetching (Hardcoded waiting time atm)

---
+++ lib/meals.ts
@@ -0,0 +1,10 @@
+import { MealItem } from "@/components/meals/meals-grid";
+import sql from "better-sqlite3";
+
+const db = sql("meals.db");
+
+export async function getMeals() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const meals = db.prepare("SELECT * from meals").all() as MealItem[];
+  return meals;
+}
