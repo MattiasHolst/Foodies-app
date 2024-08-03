b9e49d71bb31494b25b77e69caa28bcdf7be0821
Mattias <mattias@compare-it.se>
2024-08-03 16:05:15 +0200

    Added an error page

--- lib/meals.ts
+++ lib/meals.ts
@@ -6,5 +6,6 @@ const db = sql("meals.db");
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const meals = db.prepare("SELECT * from meals").all() as MealItem[];
+
   return meals;
 }
